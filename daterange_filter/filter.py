b61e2306a6f7f521c26f6326f63f4a7d84e1b58f
Maciej Januszewski <miodeq@ip-192-168-1-17.ec2.internal>
2023-08-04 12:46:46 +0200

    Update from ugettext to gettext

--- daterange_filter/filter.py
+++ daterange_filter/filter.py
@@ -11,7 +11,7 @@ import django
 from django import forms
 from django.contrib import admin
 from django.db import models
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 from django.contrib.admin.templatetags.admin_static import static
 from django.conf import settings
 
